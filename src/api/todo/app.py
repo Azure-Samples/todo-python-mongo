bc57ac34274afc0c5d850ecbb8f7de98d77e5119
azure-sdk <azuresdk@microsoft.com>
2023-05-16 18:56:11 +0000

    Synchronize repo from Repoman

--- src/api/todo/app.py
+++ src/api/todo/app.py
@@ -9,10 +9,6 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 import os
 from pathlib import Path
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
 # Use API_ALLOW_ORIGINS env var with comma separated urls like
 # `http://localhost:300, http://otherurl:100`
@@ -63,25 +59,15 @@ if settings.APPLICATIONINSIGHTS_CONNECTION_STRING:
     exporter = AzureMonitorTraceExporter.from_connection_string(
         settings.APPLICATIONINSIGHTS_CONNECTION_STRING
     )
-
-    tracer = TracerProvider(
+    tracerProvider = TracerProvider(
         resource=Resource({SERVICE_NAME: settings.APPLICATIONINSIGHTS_ROLENAME})
     )
-    tracer.add_span_processor(BatchSpanProcessor(exporter))
+    tracerProvider.add_span_processor(BatchSpanProcessor(exporter))
 
-    resource = Resource(attributes={
-        SERVICE_NAME: "your-service-name"
-    })
+    FastAPIInstrumentor.instrument_app(app, tracer_provider=tracerProvider)
 
-    provider = TracerProvider(resource=resource)
-    processor = BatchSpanProcessor(ConsoleSpanExporter())
-    provider.add_span_processor(processor)
-    trace.set_tracer_provider(provider)
-    
-    FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
-    
+
 from . import routes  # NOQA
-
 
 @app.on_event("startup")
 async def startup_event():
